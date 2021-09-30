43d6f9717d9fafd4788e5262659831af0854086f
Gelo <gelo2424@wp.pl>
2021-09-30 20:58:34 +0200

    initial commit

---
+++ src/main/java/com/gelo/virustracker/VirustrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gelo.virustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirustrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirustrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gelo/virustracker/VirustrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelo.virustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirustrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
